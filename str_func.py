b4070c649edb8db7b2e7e15c2ed4c158b7b922bf
ramazan <ramazan.dadaev.2001@mail.ru>
2024-02-27 22:31:03 +0300

    new func

--- str_func.py
+++ str_func.py
@@ -1,6 +1,9 @@
 def caps_text(text):
-  """Переводит все буквы в слове в большой регистр"""
+  """Переводит все буквы в слове в большой регистр
+  ....ffff..."""
   return text.upper()
 def title_text(text):
   """Делает первую букву каждого слова заглавным"""
   return text.title()
+def new_func():
+  return "Hello"
